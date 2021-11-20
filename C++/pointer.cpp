df8b1999b4160cabd6f32f50c1eddf1dae4c2e68
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2021-11-20 21:23:12 +0530

    Update pointer.cpp

--- C++/pointer.cpp
+++ C++/pointer.cpp
@@ -20,3 +20,25 @@ int main() {
 
     return 0;
 }
+
+
+/*
+
+THEORY 
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    a = 13;
+    int *p = &a;
+    cout<<a<<endl;  PRINTS a
+    cout<<p<<endl;  PRINTS address of a
+    cout<<*p<<endl; PRINTS a
+    cout<<*p + 1<<endl;
+    
+}
+*/
